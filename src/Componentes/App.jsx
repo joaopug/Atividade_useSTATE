78352915335bd0f233efeb615d77f289f6e57c49
joaopug <41847866+joaopug@users.noreply.github.com>
2025-09-04 22:33:00 -0300

    Atividade 1 e 2 feitas

---
+++ src/Componentes/App.jsx
@@ -0,0 +1,19 @@
+import '../App.css'
+import Likes from './Likes'
+import ControleNota from './ControleNota'
+
+function App() {
+
+
+  return (
+    <>
+
+      <Likes />
+
+      <ControleNota />
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/Votacao.jsx
@@ -0,0 +1,7 @@
+function Votacao() {
+    return (
+        <div>
+            <button onClick={votarA}>Votar A</button>
+        </div>
+    )
+}
